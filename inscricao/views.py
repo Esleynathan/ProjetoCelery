33e994811823b8102d22d3db896ed2a3d678565f
admin <esleynathan@hotmail.com>
2023-06-01 22:19:42 -0300

    Projeto prático com celery | Enviando Email

--- inscricao/views.py
+++ inscricao/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Pessoa
+from django.core.mail import send_mail
 
 def inscricao(request):
     return render(request, 'inscricao.html')
@@ -8,8 +9,8 @@ def inscricao(request):
 def processa_inscricao(request):
     nome = request.POST.get('nome')
     email = request.POST.get('email')
-
     pessoa = Pessoa(nome=nome, email=email)
     pessoa.save()
 
+    send_mail('Cadastro Confirmado.', 'Seu cadastro concluido com sucesso.', 'esleynathan@hotmail.com', recipient_list=[email])
     return HttpResponse('teste')
--- projeto_celery/settings.py
+++ projeto_celery/settings.py
@@ -128,3 +128,13 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# EMAIL
+
+DEFAULT_FROM_EMAIL = 'Esley Nathan | Curso Pythonando'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST_USER = 'esleynathan@hotmail.com'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST = 'smtp.office365.com'
